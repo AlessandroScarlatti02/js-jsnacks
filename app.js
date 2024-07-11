08806a22afddb08cbd565f505ebe00077b78d080
AlessandroScarlatti02 <alessandro.scarlatti2002@gmail.com>
2024-07-11 16:40:47 +0200

    ADD: js-snack1

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("Hello World!");
+
+//Richiesta di inserimento di 2 numeri all'utente
+const num1 = parseInt(prompt("Insierisci un numero"))
+const num2 = parseInt(prompt("Inserisci un altro numero"))
+//Recupero elemento dall'html
+const numElement = document.getElementById("num")
+//controllo di quale numero è più grande e stampa del maggiore
+if (num1 > num2) {
+    numElement.innerHTML = num1
+}
+else {
+    numElement.innerHTML = num2
+}
